711b6179163fd7b2102a94fc807e4e14e7de3e39
Maisha-Mahmood <78035756+Maisha-Mahmood@users.noreply.github.com>
2023-09-24 20:57:07 -0400

    Add files via upload

---
+++ cart_service.py
@@ -0,0 +1,30 @@
+
+from flask import Flask, jsonify
+import requests
+
+app = Flask(__name__)
+
+PRODUCT_SERVICE_URL = 'http://localhost:5000/'  
+@app.route('/cart/<int:user_id>', methods=['GET'])
+def get_cart(user_id):
+    response = requests.get(f'{PRODUCT_SERVICE_URL}/products/{user_id}')
+    if response.status_code == 200:
+        products = response.json()
+        return jsonify({'user_id': user_id, 'cart': []}) 
+    else:
+        return jsonify({'message': 'Failed to retrieve cart'}), 500
+
+@app.route('/cart/<int:user_id>/add/<int:product_id>', methods=['POST'])
+def add_to_cart(user_id, product_id):
+    response = requests.get(f'{PRODUCT_SERVICE_URL}/products/{product_id}')
+    if response.status_code == 200:
+        product_data = response.json()
+        return jsonify({'message': 'Product added to cart'}), 201 
+    else:
+        return jsonify({'message': 'Failed to add product to cart'}), 500
+
+@app.route('/cart/<int:user_id>/remove/<int:product_id>', methods=['POST'])
+def remove_from_cart(user_id, product_id):
+    return jsonify({'message': 'Product removed from cart'}), 200  
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ product_service.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+products = [
+    {"id": 1, "name": "Milk", "price": 0.6, "quantity": 100},
+    {"id": 2, "name": "Eggs", "price": 0.2, "quantity": 160},
+    {"id": 3, "name": "Bread", "price": 0.3, "quantity": 100},
+]
+
+@app.route('/products', methods=['GET'])
+def get_products():
+    return jsonify(products)
+
+@app.route('/products/<int:product_id>', methods=['GET'])
+def get_product(product_id):
+    product = next((p for p in products if p["id"] == product_id), None)
+    if product:
+        return jsonify(product)
+    else:
+        return jsonify({"message": "Product not found"}), 404
+
+@app.route('/products', methods=['POST'])
+def add_product():
+    data = request.get_json()
+    if "name" in data and "price" in data and "quantity" in data:
+        new_product = {
+            "id": len(products) + 1,
+            "name": data["name"],
+            "price": data["price"],
+            "quantity": data["quantity"],
+        }
+        products.append(new_product)
+        return jsonify({"message": "Product added successfully"}), 201
+    else:
+        return jsonify({"message": "Invalid data. Make sure to include name, price, and quantity."}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
